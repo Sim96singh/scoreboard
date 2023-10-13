fcf7b0ecac52e1deebb6d3970b38d4dfbcd0abd6
Simpy Singh <107769149+Sim96singh@users.noreply.github.com>
2023-10-12 21:37:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ let count=0;
 let basescore= document.getElementById("homenum")
 let basescoreguest= document.getElementById("guestnum")
 
-function onepoint(){
-   
-
-  count= count+1;   
-    basescore.innerText=count;
+function onepoint(){  
+   count= count+1;   
+   basescore.innerText=count;
 }
 
 function twopoint(){
-   
-   count= count+2;   
+    count= count+2;   
     basescore.innerText=count;
 }
 
 function threepoint(){
-   
     count= count+3;   
     basescore.innerText=count;
 }
@@ -27,22 +23,20 @@ function reset(){
 }
 
 let guestcount=0
-function onepointgt(){  
 
+function onepointgt(){ 
   guestcount= guestcount+1;   
-basescoreguest.innerText=guestcount;
+  basescoreguest.innerText=guestcount;
 }
 
 function twopointgt(){
-   
- guestcount= guestcount+2;   
- basescoreguest.innerText=guestcount;
-     }
+   guestcount= guestcount+2;   
+   basescoreguest.innerText=guestcount;
+ }
 
 function threepointgt(){
-   
-  guestcount= guestcount+2;   
-basescoreguest.innerText=guestcount;
+   guestcount= guestcount+2;   
+   basescoreguest.innerText=guestcount;
 }
 
 function resetguest(){
